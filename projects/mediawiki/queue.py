9f6e0c3110fa197d4e1efc8ffe170149955beb6c
Ben Liyanage <ben@perfectresolution.com>
2014-11-01 19:02:47 -0700

    in the middle of adding queues

--- projects/mediawiki/queue.py
+++ projects/mediawiki/queue.py
@@ -4,13 +4,7 @@ import time
 class Queue:
     """An asynchronous queuing library."""
     
-    _parallelism = 1
-    _tasks = []
-    _jobs = []
-    _isRunning = False
-    
-    def __init__(self, parallelism):
-    
+    def __init__(self, parallelism=1):
     #* Constructs a new Queue.
     #* @constructor
     #* @param {Number} [parallelism=1] Maximum number of tasks to run in parallel;
@@ -20,10 +14,13 @@ class Queue:
             raise Exception("parallelism cannot be less than 1")
             
         self._parallelism = parallelism
+        self._tasks = multiprocessing.Queue()
+        self._jobs = multiprocessing.Queue()
+        self._isRunning = False
     
     def size(self):
         #* @returns {Number} Number of tasks awaiting execution. Does not count any tasks that are in flight
-        return len(self._tasks)
+        return len(self._tasks.qsize())
         
     def isRunning(self):
         #* @returns {Boolean} Whether the queue is running or not.
@@ -33,7 +30,7 @@ class Queue:
     def inFlight(self):
         #* @returns {Number} Number of tasks currently executing.
         
-        return len(self._jobs)
+        return len(self._jobs.qsize())
         
     def addTask(self, function):
         # * Adds a new task to the queue. When executed, the task will be passed a callback used to signal the task has completed:
@@ -43,7 +40,7 @@ class Queue:
         # * @param {Function} task Function which begins the asynchronous task,
         # * taking a callback to be invoked upon task completion.
         # * @returns The queue object.
-        self._tasks.append(function)
+        self._tasks.put(function)
         
     def start(self):
         # * Begin executing queued tasks.
@@ -64,24 +61,34 @@ class Queue:
         if self._isRunning:
             return
         
-        self._isRunning = True
+        #fire up our processes
+        for x in range(1, self._parallelism):
+            p = multiprocessing.Process(target = self.startJobs, args=(self._isRunning,))
+            p.start()
+        
+        #this is done in a process so that we can query the status of the queue asynchronously
+        p = multiprocessing.Process(target = self.startJobs, args=(self._isRunning,))
+        p.start()
+    
+    def startJobs(self, _isRunning):
+        # function for executing jobs asynchronously
+        _isRunning = True
         
         while len(self._tasks) > 0:
         
-            p = multiprocessing.Process(target = task.pop())
-            jobs.append(p)
+            p = multiprocessing.Process(target = self._tasks.pop(0))
+            self._jobs.append(p)
             p.start()
             
             # wait until a parallel job  becomes available
-            while len(_jobs) >= self._parallelism:
+            while len(self._jobs) >= self._parallelism:
                 #let's wait a bit to give them a chance to finish
                 time.sleep(1)
                 
                 # check if any of the jobs are finished
-                for (i, job) in enumerate(jobs):
+                for (i, job) in enumerate(self._jobs):
                     if not job.is_alive():
                         # this job is finished, remove it from our inflight jobs
-                        jobs.pop(i)
+                        self._jobs.pop(i)
 
         self._isRunning = False
-        
--- projects/mediawiki/queue_tests.py
+++ projects/mediawiki/queue_tests.py
@@ -5,7 +5,7 @@ import time
 class QueueTests(unittest.TestCase):
 
     def testConstructor(self):
-        q = Queue(1)
+        q = Queue()
         self.assertEqual(1, q._parallelism)
         
         self.assertRaises(Exception, Queue, 0)
@@ -13,20 +13,68 @@ class QueueTests(unittest.TestCase):
         self.assertFalse(q.isRunning())
         self.assertEqual(0, q.inFlight())
         
-        q.addTask(self.waitOneSecond)
-        self.assertEqual(1, q.size())
-        
-        
     def testSize(self):
         
-        q2 = Queue(1)
+        q = Queue(1)        
+        self.assertEqual(0, q.size())
         
-        print q2.size()
-        print q2._tasks
-        #self.assertEqual(0, q.size())
+        q.addTask(self.waitOneSecond)
+        self.assertEqual(1, q.size())
+    
+    def testStart(self):
+        q = Queue()
         
-    def waitOneSecond(self):
+        q.addTask(self.waitOneSecond)
+        q.addTask(self.waitFiveSeconds)
+        q.addTask(self.waitOneSecond)
+        self.assertEqual(3, q.size())
+        self.assertFalse(q.isRunning())
+        self.assertEqual(0, q.inFlight())
+        print q._parallelism
+        
+        q.start()
         time.sleep(1)
+        #print q._jobs
+        #self.assertEqual(1, q.inFlight())
+        #self.assertEqual(2, q.size())
+        self.assertTrue(q.isRunning())
+        time.sleep(1)
+        self.assertEqual(1, q.inFlight())
+        self.assertEqual(1, q.size())
+        self.assertTrue(q.isRunning())
+        time.sleep(5)
+        self.assertEqual(1, q.inFlight())
+        self.assertEqual(0, q.size())
+        self.assertTrue(q.isRunning())
+        time.sleep(1)
+        self.assertEqual(0, q.inFlight())
+        self.assertEqual(0, q.size())
+        self.assertFalse(q.isRunning())
+    
+    # def testStartErrorHandling(self):
+        # q = Queue()
+        
+        # q.addTask(self.waitOneSecond)
+        # q.addTask(self.raiseException)
+        # q.addTask(self.waitFiveSeconds)
+        # q.addTask(self.waitOneSecond)
+        
+        # q.start()
+    
+    
+    def waitOneSecond(self):
+        print "waitOneSecond: Begin"
+        time.sleep(1)
+        print "waitOneSecond: End"
+    
+    def waitFiveSeconds(self):
+        print "waitFiveSeconds: Begin"
+        time.sleep(5)
+        print "waitOneSecond: End"
+    
+    def raiseException(self):
+        print "raiseException"
+        raise Exception("Let the Queue Continue!")
 
 if __name__ == '__main__':
     unittest.main()
