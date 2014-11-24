0e8d11d8610a075c5e29107c071e0b7bf7e421d5
mousterian <rivladi@gmail.com>
2014-11-23 19:24:29 -0800

    added static files and rendered a basic index file with a list of all foods

---
+++ projects/farmtest/farmlyvore/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from farmlyvore import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+
+)
+urlpatterns += staticfiles_urlpatterns()
--- projects/farmtest/farmlyvore/views.py
+++ projects/farmtest/farmlyvore/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+import json, logging
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404, HttpResponseRedirect, HttpResponseBadRequest
+from django.core.urlresolvers import reverse
+from django.views import generic
+from farmlyvore.models import Food
 
-# Create your views here.
+def index(request):
+    food_list = Food.objects.all()#.order_by('food')[:5]
+    context = { 'food_list': food_list }
+    return render(request, 'farmlyvore/index.html', context)
